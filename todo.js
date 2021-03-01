91476b32e70b0413663af44e926b60a5db53067d
Julien <menten.julien@outlook.be>
2021-03-01 13:12:11 +0100

    Added .htaccess for visible on Apache server (one.com)

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
 /*
     TODO: Nav - Wanneer men op de landing page of portal zit -> invisible achtegrond en witte nav
     TODO: Navigatie - Bij clicken op contact -> scrollen naar contact deel van de homepage
-    ! ERROR: https://stackoverflow.com/questions/56248347/how-to-fix-404-error-when-directly-accessing-a-component-in-react  / https://create-react-app.dev/docs/deployment/#serving-apps-with-client-side-routing
 */
